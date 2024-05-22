559bea6cf1b131c4e6da98fbc74dbbc20384da0e
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2024-05-22 10:34:21 +0530

    datatyeps in js

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+//-------differenrt types of data types in JS----------
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness mainly in React
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
