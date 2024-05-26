a717d7a04077f2db17fc215c0e3e5eeecac088f0
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2024-05-26 10:19:15 +0530

    added symbol data types useCase

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -27,4 +27,22 @@ console.log(typeof undefined); // undefined
 console.log(typeof null); // object
 
 
+// ******** SYMBOL Uses (Interview Specific)------->
+//In JavaScript, Symbols themselves don't hold values in the traditional sense like numbers or strings. They are 
+// unique identifiers and remain immutable throughout the program. However, you can associate data with Symbols indirectly using a few approaches:
+// You can associate data with Symbols using object properties, Map.
 
+//1.Object Properties:Create an object and use the Symbol as a property key. The value you assign to that property becomes associated with the Symbol.
+// In this example, the Symbol mySymbol is used as a unique key to store an object containing user data.
+const mySymbol = Symbol('user-data');
+const user = {
+  name: 'Alice',
+  [mySymbol]: { age: 30, city: 'New York' }
+};
+console.log(user[mySymbol]); // { age: 30, city: 'New York' }
+
+//2. Map Data Structure:Use a Map data structure, which allows storing key-value pairs where keys can be any data type, including Symbols.
+const mySymbol2 = Symbol('user-preferences');
+const preferences = new Map();
+preferences.set(mySymbol2, { darkMode: true, language: 'en' });
+console.log(preferences.get(mySymbol2)); // { darkMode: true, language: 'en' }
