391d5c9db9a31f1338abbe0cecf577c81a5c01e0
RATNESH MAURYA <ratneshmaurya2001@gmail.com>
2024-06-01 18:13:37 +0530

    Project on removing the section of DOM on clicking it (Great Fundamental)

--- 03_events_async_promises/02_remove_onClick_eventProject.js
+++ 03_events_async_promises/02_remove_onClick_eventProject.js
@@ -1,19 +1,19 @@
 
 
 
-// <body style="background-color: #414141; color: aliceblue;">
-//     <h2>Amazing image</h2>
-//     <div >
-//         <ul id="images">
-//             <li><img width="200px" id="photoshop" src="https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="photoshop"></li>
-//             <li><img width="200px" id="japan" src="https://images.pexels.com/photos/3532553/pexels-photo-3532553.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
-//             <li><img width="200px" id="river" src="https://images.pexels.com/photos/3532551/pexels-photo-3532551.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
-//             <li><img width="200px" id="owl" src="https://images.pexels.com/photos/3532552/pexels-photo-3532552.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" ></li>
-//             <li><img width="200px" id="prayer" src="https://images.pexels.com/photos/2522671/pexels-photo-2522671.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
-//             <li><a style="color: aliceblue;" href="https://google.com" id="google">Google</a></li>
-//         </ul>
-//    </div>
-// </body>
+{/* <body style="background-color: #414141; color: aliceblue;">
+    <h2>Amazing image</h2>
+    <div >
+        <ul id="images">
+            <li><img width="200px" id="photoshop" src="https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="photoshop"></li>
+            <li><img width="200px" id="japan" src="https://images.pexels.com/photos/3532553/pexels-photo-3532553.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
+            <li><img width="200px" id="river" src="https://images.pexels.com/photos/3532551/pexels-photo-3532551.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
+            <li><img width="200px" id="owl" src="https://images.pexels.com/photos/3532552/pexels-photo-3532552.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" ></li>
+            <li><img width="200px" id="prayer" src="https://images.pexels.com/photos/2522671/pexels-photo-2522671.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt=""></li>
+            <li><a style="color: aliceblue;" href="https://google.com" id="google">Google</a></li>
+        </ul>
+   </div>
+</body> */}
 
 
 // ********* IMPORTANT for Event Listener Activation----->> as below we are selecting parent and adding event on parent---->
@@ -37,6 +37,15 @@
     //removeIt.parentNode.removeChild(removeIt) --> also be used to remove the child, using removeChild() function
 
 
+    // ***** We can also do this project easily by---> first add the id="toRemove" to each <li> to be accessible easily
+    const liIteams=document.querySelectorAll("#toRemove"); //this gives all list items in it as a nodelist
+        liIteams.forEach((Iteam)=>{
+        Iteam.addEventListener('click',()=>{
+            Iteam.remove();
+        })
+    })
+
+    
 
     //************** Why this project was a little unique??
     // As we are selecting parent and adding event on parent(i.e on all its child, not on a specific child)
@@ -49,4 +58,3 @@
 
 
 
-    
